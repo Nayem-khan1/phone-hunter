6516beebfc84f70116b7d61520bf89184d9c9c3d
Nayem-khan1 <nayemkhan4496@gmail.com>
2022-12-05 13:21:26 +0600

    complete site

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const loadPhone = async(search) => {
+const loadPhone = async(search, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${search}`;
     const resposnse = await fetch(url);
     const data = await resposnse.json();
-    displayData(data.data);
+    displayData(data.data, dataLimit);
 }
-const displayData = (allData) => {
-    allData = allData.slice(0, 9);
+const displayData = (allData, dataLimit) => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
+    //show all
+    const showAll = document.getElementById('show-all');
+    
+    if(dataLimit && allData.length > 9){
+        allData = allData.slice(0, 9);
+        showAll.classList.remove('d-none');
+    }else{
+        showAll.classList.add('d-none');
+    }
+    
+    //display not found
     const notFoundMsg = document.getElementById('not-found-msg');
-    if(allData.length == 0){
+    if(allData.length === 0){
         notFoundMsg.classList.remove('d-none');
     }
     else{
@@ -24,28 +34,58 @@ const displayData = (allData) => {
             <div class="card-body">
                 <h5 class="card-title">${data.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="phoneDetails('${data.slug}')" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
             </div>
         </div>
         `
         phoneContainer.appendChild(phoneDiv);
-        console.log(data);
     })
 
     toggoleSpenner(false);
 }
-const search = () =>{
+const search = (dataLimit) =>{
     toggoleSpenner(true);
-    const searchBtn = document.getElementById('search-btn');
     const searchFild = document.getElementById('search-fild');
     const searchResult = searchFild.value;
-    searchFild.value = '';
-    loadPhone(searchResult);
-}
+    loadPhone(searchResult, dataLimit);
+} 
+document.getElementById('search-btn').addEventListener('click', function(){
+    search(10);
+})
+document.getElementById('search-fild').addEventListener('keypress', function(event){
+    if(event.key == 'Enter'){
+        search(10);
+    }
+})
 const toggoleSpenner = (isLoading) => {
     const loaderElement = document.getElementById('loader');
-    if(isLoading === true){
+    if(isLoading){
         loaderElement.classList.remove('d-none');
     }else{
         loaderElement.classList.add('d-none');
     }
+}
+
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    search();
+})
+
+const phoneDetails = (id) =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhoneDetails(data.data))
+}
+const displayPhoneDetails = data => {
+    const phoneName = document.getElementById('phoneDetailModalLabel');
+    const modalDiv = document.getElementById('modal-div');
+    modalDiv.innerHTML = `
+        <p>Release Date: ${data.releaseDate ? data.releaseDate : 'No Release Date Found'}</p>
+        <p>Chip Set: ${data.mainFeatures.chipSet}</p>
+        <p>Display Size: ${data.mainFeatures ? data.mainFeatures.displaySize : 'Not found'}</p>
+        <p>Memory: ${data.mainFeatures.memory}</p>
+
+    `
+    phoneName.innerText = `${data.name}`;
+    console.log(data);
 }
